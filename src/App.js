1ce4110fd004831b36fc87f0b8f1951cda39e750
jjjggg111 <thisisjungjiwon@gmail.com>
2021-01-10 18:38:22 +0900

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Amazon Clone</h1>
+      <h2></h2>
+      
+    </div>
+  );
+}
+
+export default App;
