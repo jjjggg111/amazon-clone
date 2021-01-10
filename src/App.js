55b2b13b14e5ff5485d3f88b891d5a5e6446889b
jjjggg111 <thisisjungjiwon@gmail.com>
2021-01-10 22:36:56 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Amazon Clone</h1>
-      <h2></h2>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <Header />
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+{
+  /*localhost.com*/
+}
+{
+  /*localhost.com/checkout*/
+}
+{
+  /*localhost.com/login*/
 }
 
 export default App;
